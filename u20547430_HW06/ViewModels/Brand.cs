8f86dfb84647ab89681d76e07481447da3f4356e
20547430 <u20547430@tuks.co.za>
2022-10-30 12:50:27 +0200

    Brand Model

---
+++ u20547430_HW06/ViewModels/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u20547430_HW06.ViewModels
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        
+    }
+}
