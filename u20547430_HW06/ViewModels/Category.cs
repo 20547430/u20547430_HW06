8460364f24024d6ad92489c70e31c4ebfd797edc
20547430 <u20547430@tuks.co.za>
2022-10-30 12:52:14 +0200

    Category Model

---
+++ u20547430_HW06/ViewModels/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u20547430_HW06.ViewModels
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
